46da6a6cfac8dbf652c44fe6677294650dfb7658
Shivam Shukla <shuklashivam100803@gmail.com>
2025-08-07 12:25:16 +0530

    6th Commit

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,7 @@
 import sys # Jo exception aayega vo sys module ke pass rahega
 import logging
+from src.logger import logging
+
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info() # here exc_tb used to give info kis jagah kis line me error aaya hai
@@ -21,10 +23,10 @@ class CustomException(Exception):  # Own custom exception class
         return self.error_message
     
 
-# if __name__=="__main__":
+if __name__=="__main__":
 
-#     try:
-#         1/0
-#     except Exception as e:
-#         logging.info("Divide by Zero Error!")
-#         raise CustomException(e,sys)
+    try:
+        1/0
+    except Exception as e:
+        logging.info("Divide by Zero Error!")
+        raise CustomException(e,sys)
